541fe857895d5fb4c1aa3d6d8ad9c14c4599ea9b
estevesnp <nuno.pinto@live.com>
2025-01-26 18:00:34 +0000

    updated build to export module

--- build.zig
+++ build.zig
@@ -5,34 +5,22 @@ pub fn build(b: *std.Build) void {
 
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "zttp",
+    const module = b.addModule("zttp", .{
         .root_source_file = b.path("src/lib.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    b.installArtifact(lib);
-
     {
-        const check_lib = b.addStaticLibrary(.{
-            .name = "zttp",
-            .root_source_file = b.path("src/lib.zig"),
-            .target = target,
-            .optimize = optimize,
-        });
-
         const check_exe = b.addExecutable(.{
             .name = "zttp",
             .root_source_file = b.path("examples/simple_server.zig"),
             .target = target,
             .optimize = optimize,
         });
-
-        check_exe.root_module.addImport("zttp", &check_lib.root_module);
+        check_exe.root_module.addImport("zttp", module);
 
         const check_step = b.step("check", "Check that the program compiles");
-        check_step.dependOn(&check_lib.step);
         check_step.dependOn(&check_exe.step);
     }
 
@@ -43,7 +31,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    exe.root_module.addImport("zttp", &lib.root_module);
+    exe.root_module.addImport("zttp", module);
 
     b.installArtifact(exe);
 
